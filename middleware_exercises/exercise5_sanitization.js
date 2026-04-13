e533c929d166c7a277dd8400ae477aff16e5fdac
Harsh Agrawal <Harshagrawal3375@gmail.com>
2026-04-13 10:42:29 +0530

    Added

--- middleware_exercises/exercise5_sanitization.js
+++
@@ -1,9 +0,0 @@
-const xss = require('xss-clean');
-const mongoSanitize = require('express-mongo-sanitize');
-
-const setupSanitization = (app) => {
-    app.use(xss());
-    app.use(mongoSanitize());
-};
-
-module.exports = setupSanitization;
